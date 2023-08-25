d008bc19ad9c2980b48b07e810a4c77f7c9bf267
Paulo Roberto <paulonrss+git@gmail.com>
2023-08-25 05:21:00 -0300

    Fix Testing of Http Post, Creation more Test of Http Post, Creation Test of Http Get and Creation test of put with return sucess

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -65,46 +65,167 @@ void main() {
         () async {
       client.mockPost(400, body: jsonEncode(body));
 
-      final response = httpAdapter.request(url: url, method: method);
-
-      expect(response, throwsA(HttpError.badRequest));
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.badRequest));
     });
 
     test("Should return unauthorized if post returns 401 with invalid data",
         () async {
       client.mockPost(401, body: jsonEncode(body));
 
-      final response = httpAdapter.request(url: url, method: method);
-
-      expect(response, throwsA(HttpError.unauthorized));
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.unauthorized));
     });
 
     test("Should return notFound if post returns 404 with invalid data",
         () async {
       client.mockPost(404, body: jsonEncode(body));
 
-      final response = httpAdapter.request(url: url, method: method);
-
-      expect(response, throwsA(HttpError.notFound));
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.notFound));
     });
 
     test("Should return forbidden if post returns 403 with invalid data",
         () async {
       client.mockPost(403, body: jsonEncode(body));
 
-      final response = httpAdapter.request(url: url, method: method);
-
-      expect(response, throwsA(HttpError.forbidden));
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.forbidden));
     });
 
     test("Should return serverError if post returns 500 with invalid data",
         () async {
-      //
       client.mockPost(500, body: jsonEncode(body));
 
-      final response = httpAdapter.request(url: url, method: method);
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.serverError));
+    });
 
-      expect(response, throwsA(HttpError.serverError));
+    test('Should return ServerError if post throws', () async {
+      client.mockPostError();
+
+      final future = httpAdapter.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+
+  group('get', () {
+    const method = 'get';
+    test("Should request with correct values", () async {
+      await httpAdapter.request(url: url, method: method);
+      verify(() => client.get(
+            Uri.parse(url),
+            headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json'
+            },
+          )).called(1);
+
+      await httpAdapter.request(
+          url: url, method: 'get', headers: {'any_header': 'any_value'});
+
+      verify(() => client.get(Uri.parse(url), headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any_header': 'any_value'
+          }));
+    });
+
+    test("Should return data if returns 200", () async {
+      client.mockGet(200, body: jsonEncode(body));
+      final response = await httpAdapter.request(url: url, method: method);
+      expect(response, body);
+    });
+
+    test("Should return null if returns 200 with no data", () async {
+      client.mockGet(200, body: "");
+      final response = await httpAdapter.request(url: url, method: method);
+      expect(response, null);
+    });
+
+    test("Should return BadRequest if get retuns 400", () async {
+      client.mockGet(400);
+
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.badRequest));
+    });
+
+    test("Should return Unauthorized if get retuns 401", () async {
+      client.mockGet(401);
+
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.unauthorized));
+    });
+
+    test("Should return forbidenn if get retuns 403", () async {
+      client.mockGet(403);
+
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.forbidden));
+    });
+
+    test("Should return notFound if get retuns 404", () async {
+      client.mockGet(404);
+
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.notFound));
+    });
+
+    test("Should return serverError if get retuns 500", () async {
+      client.mockGet(500);
+
+      expect(() async {
+        await httpAdapter.request(url: url, method: method);
+      }, throwsA(HttpError.serverError));
+    });
+  });
+  group('put', () {
+    const method = 'put';
+    test("Should call put with correct values", () async {
+      await httpAdapter.request(url: url, method: method, body: body);
+
+      verify(() => client.put(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: jsonEncode(body)));
+    });
+    test("Should return the body if the returns contains values ", () async {
+      client.mockPut(200, body: jsonEncode(body));
+
+      final response =
+          await httpAdapter.request(url: url, method: method, body: body);
+
+      expect(response, body);
+    });
+
+    test("Should return null if the returns does not contain values", () async {
+      client.mockPut(200, body: '');
+
+      final response =
+          await httpAdapter.request(url: url, method: method, body: body);
+
+      expect(response, null);
+    });
+
+    test("Should return null if the returns 204", () async {
+      client.mockPut(204, body: jsonEncode(body));
+
+      final response =
+          await httpAdapter.request(url: url, method: method, body: body);
+
+      expect(response, null);
     });
   });
 }
